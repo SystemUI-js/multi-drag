abfc5a53f402ea7c7b94d41886952ff4e3bc3721
ZX <wszxdhr@126.com>
2025-08-07 17:48:06 +0800

    chore: 修复打包之后Github Pages展示错误

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/multi-drag/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [
     checker({
       typescript: true,
